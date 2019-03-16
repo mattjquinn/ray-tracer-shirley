296f9206b52e4061ecbf27440a432d94789e9ec2
Matt Quinn <matt@mattjquinn.com>
2019-03-16 12:35:31 -0400

    Up to Chapter 4.

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,4 +1,4 @@
-use std::ops::Index;
+use std::ops::{Index, Mul, Add, Div};
 
 pub struct Vec3 {
     e : [f64; 3],
@@ -15,6 +15,14 @@ impl Vec3 {
     pub const fn r(&self) -> f64 { self.e[0] }
     pub const fn g(&self) -> f64 { self.e[1] }
     pub const fn b(&self) -> f64 { self.e[2] }
+
+    pub fn length(&self) -> f64 {
+        (self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]).sqrt()
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+         self / self.length()
+    }
 }
 
 impl Index<usize> for Vec3 {
@@ -22,5 +30,44 @@ impl Index<usize> for Vec3 {
 
     fn index(&self, idx: usize) -> &f64 {
         &self.e[idx]
+    }
+}
+
+impl<'a, 'b> Add<&'a Vec3> for &'b Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: &'a Vec3) -> Vec3 {
+        Vec3::new(self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2])
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Vec3 {
+        Vec3::new(self[0] / rhs, self[1] / rhs, self[2] / rhs)
+    }
+}
+
+impl Mul<&Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: &Vec3) -> Vec3 {
+        Vec3::new(self * rhs[0], self * rhs[1], self * rhs[2])
+    }
+}
+
+pub struct Ray<'a> {
+    a : &'a Vec3,
+    b : &'a Vec3,
+}
+
+impl<'a> Ray<'a> {
+    pub fn new(a : &'a Vec3, b : &'a Vec3) -> Ray<'a> {
+        Ray { a, b }
+    }
+
+    pub fn direction(&self) -> &'a Vec3 {
+        self.b
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,35 @@
 
 mod lib;
 
-use lib::Vec3;
+use lib::{Vec3, Ray};
 
 fn main() {
     let nx = 200;
     let ny = 100;
     println!("P3\n{} {}\n255", nx, ny);
+
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new(
-                i as f64 / nx as f64,
-                j as f64 / ny as f64,
-                0.2,
-            );
+            let u = i as f64 / nx as f64;
+            let v = j as f64 / ny as f64;
+            let end = &lower_left_corner + &(&(u * &horizontal) + &(v * &vertical));
+            let r = Ray::new(&origin, &end);
+            let col = color(&r);
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
+}
+
+fn color(r : &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    &((1.0 - t) * &Vec3::new(1.0, 1.0, 1.0))  + &(t * &Vec3::new(0.5, 0.7, 1.0))
 }
